b0bbafede28ea7a25d1638c7c60a2b117902010b
KonradSpiewak <konradspiewak2@gmail.com>
2025-01-20 19:17:10 +0100

    Add files via upload

---
+++ 2024_17_22/src/Level_Crossing.java
@@ -0,0 +1,2 @@
+public class Level_Crossing {
+}
---
+++ 2024_17_22/src/Railway_System.java
@@ -0,0 +1,27 @@
+public class Railway_System {
+
+     public void add (Axel_Counter axelCounter){
+         System.out.println("Added axelCounter");
+     }
+
+    public void add (Car car){
+        System.out.println("Added car");
+    }
+
+    public void add (Locomotive locomotive){
+        System.out.println("Added locomotive");
+    }
+
+    public void add (Locomotive locomotive, Level_Crossing levelCrossing){
+        System.out.println("Added locomotive and levelCrossing");
+    }
+
+    public void add (Locomotive locomotive, Level_Crossing levelCrossing, Axel_Counter axelCounter){
+        System.out.println("Added locomotive and levelCrossing and axelCounter");
+
+    }
+
+
+
+
+}
